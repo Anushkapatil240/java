6d674e45542be8ffe89ef113ca061852aabc651c
Anushkapatil240 <anushkapatil240@gmail.com>
2024-09-02 18:48:46 +0530

    Add files via upload

---
+++ Day.java
@@ -0,0 +1,51 @@
+import java.lang.*;
+import java.util.*;
+
+public class Day 
+{
+    public static void main(String[] args)
+    {
+        int Day ;
+        String dayString;
+		
+		Scanner S = new Scanner(System.in);
+		
+		System.out.print("\n Enter Day Number : ");
+		Day = S.nextInt();
+		
+        switch (Day) 
+		{
+			case 1:
+				dayString = "Monday";
+				break;
+				
+			case 2:
+				dayString = "Tuesday";
+				break;
+
+			case 3:
+				dayString = "Wednesday";
+				break;
+
+			case 4:
+				dayString = "Thursday";
+				break;
+
+			case 5:
+				dayString = "Friday";
+				break;
+
+			case 6:
+				dayString = "Saturday";
+				break;
+
+			case 7:
+				dayString = "Sunday";
+				break;
+
+			default:
+				dayString = "Invalid Day";
+        }
+		
+    }
+}
