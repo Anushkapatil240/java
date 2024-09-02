3c09e322085c38b01c05cba61b1ad178a9843def
Anushkapatil240 <anushkapatil240@gmail.com>
2024-09-02 18:53:55 +0530

    Add files via upload

---
+++ Size.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+public class Size {
+    public static void main (String [] args){
+
+        System.out.print("\n The size of int : " + Integer.SIZE + " bits ");
+
+        System.out.print("\n The size of char : " + Character.SIZE + " bits ");
+
+        System.out.print("\n The size of double : " + Double.SIZE + " bits ");
+
+        System.out.print("\n The size of float : " + Float.SIZE + " bits ");
+
+        System.out.print("\n The size of byte : " + Byte.SIZE + " bits ");
+
+        System.out.print("\n The size of short : " + Short.SIZE + " bits ");
+
+        System.out.print("\n The size of long : " + Long.SIZE + " bits ");
+    }
+    
+}
