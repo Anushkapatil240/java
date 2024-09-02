159c9666fa0a2430ab87e8d9937becb98a5dfdd2
Anushkapatil240 <anushkapatil240@gmail.com>
2024-09-02 18:54:21 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+import java.util.Scanner;
+public class Student
+ {
+    public static void main(String[]args)
+    {
+        String Name , Course , City;
+        int RollNumber;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\n Enter Name: ");
+        Name = sc.nextLine();
+
+        System.out.print("\n Enter Course: ");
+        Course = sc.nextLine();
+
+        System.out.print("\n Enter City: ");
+        City = sc.nextLine();
+
+        System.out.print("\n Enter  RollNumber: ");
+        RollNumber= sc.nextInt();
+
+        System.out.print("\n Thanks for details");
+
+    }
+       
+}
